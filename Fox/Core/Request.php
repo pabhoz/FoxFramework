aa77cc84cc2b72fa9611b25ec3a9013a4fb6f219
Pabhoz <pabhoz@gmail.com>
2018-04-11 18:46:40 -0500

    Added CORS support to Core/Request

--- Fox/Core/Request.php
+++ Fox/Core/Request.php
@@ -11,7 +11,20 @@ class Request {
     private $status;
  
     public function __construct() {
+        
+        // Access-Control headers are received during OPTIONS requests
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])){
+                // may also be using PUT, PATCH, HEAD etc
+                header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");         
+            }
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])){
+               header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");//{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}"); 
+            }
+            exit(0);
+        }
+        
         $this->method = strtolower($_SERVER["REQUEST_METHOD"]);
         $this->phpinput = file_get_contents('php://input');
         
